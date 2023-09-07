b4a7e48a19913f4a7594788f450e7e3f84b67060
heko <91366057+hkohko@users.noreply.github.com>
2023-09-07 10:33:08 +0700

    created sample_dir for easy testing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
 from pyloudnorm import meter
-from numpy import ndarray
-from pathlib import PurePath
+from numpy import ndarray, transpose
+from pathlib import PurePath, Path
 from scipy.io import wavfile
 from warnings import filterwarnings
 from pedalboard.io import AudioFile
@@ -9,39 +8,48 @@ from pedalboard.io import AudioFile
 filterwarnings("ignore")
 
 _MAIN_DIR = PurePath(__file__).parents[0]
-SAMPLE = _MAIN_DIR.joinpath("sample.wav")
-SAMPLE_2 = _MAIN_DIR.joinpath("sample2.mp3")
+SAMPLE_DIR = _MAIN_DIR.joinpath("samples")
 
 
-def read_wav(filename: str) -> tuple[int, ndarray]:
+def read_wav(filename: str) -> tuple[int, ndarray, str]:
     file = _MAIN_DIR.joinpath(filename)
     file_path = str(file)
-
+    # use scipy if audio file is .wav
     if file.suffix == ".wav":
         wav = wavfile.read(file_path)
-        return wav
-
+        sr = wav[0]
+        data = wav[1]
+        return sr, data, file
+    # else use pedalboard
     with AudioFile(file_path) as audio_file:
-        sr = audio_file.samplerate
+        sr: int = audio_file.samplerate
         data = audio_file.read(audio_file.frames)
-        reshaped_data = np.transpose(data)
-        return sr, reshaped_data
+        reshaped_data = transpose(data)
+        return sr, reshaped_data, file
 
 
-def sample_rate(wav: tuple[int, ndarray]) -> int:
+def sample_rate(wav: tuple[int, ndarray, PurePath]) -> int:
     return wav[0]
 
 
-def audio_data(wav: tuple[int, ndarray]) -> ndarray:
+def audio_data(wav: tuple[int, ndarray, PurePath]) -> ndarray:
     return wav[1]
+
+
+def file_ext(wav: tuple[int, ndarray, PurePath]) -> str:
+    return wav[2].suffix
+
+
+def file_name(wav: tuple[int, ndarray, PurePath]) -> str:
+    return wav[2].stem
 
 
 def audio_channel(data: ndarray) -> int:
     return data.shape[1]
 
 
-def measure_lufs(rate: int, data: ndarray) -> float:
-    create_meter = meter.Meter(rate)
+def measure_lufs(sr: int, data: ndarray) -> float:
+    create_meter = meter.Meter(sr)
     loudness = create_meter.integrated_loudness(data)
     return loudness
 
@@ -50,18 +58,29 @@ def audio_seconds(sr: int, data: ndarray) -> float:
     return f"{(len(data) / sr):.2f}"
 
 
-def basic_main(wav: tuple[int, ndarray]):
-    sr = sample_rate(wav)
-    data = audio_data(wav)
-    channels = audio_channel(data)
-    loudness = measure_lufs(sr, data)
+def basic_main(wav: tuple[int, ndarray, str]):
+    sr = sample_rate(wav)  # (in kHz)
+    data = audio_data(wav)  # raw audio data, as numpy.ndarray
+    name = file_name(wav)
+    ext = file_ext(wav)
+    channels = audio_channel(data)  # mono, stereo, etc
+    loudness = f"{measure_lufs(sr, data):.2f}"  # ITU-R BS.1770 compliant
     length = audio_seconds(sr, data)
-    print(f"Audio Channel(s): {channels}")
-    print(f"Loudness: {loudness:.2f}dB LUFS")
-    print(f"Length: {length} seconds")
+
+    display = [
+        f"Name: {name}",
+        f"File extension: {ext}",
+        f"Length: {length} seconds",
+        f"Audio Channel(s): {channels}",
+        f"Sample Rate: {sr} kHz",
+        f"Loudness: {loudness}dB LUFS",
+        "",
+    ]
+
+    print("\n".join(display))
 
 
 if __name__ == "__main__":
-    reads = [read_wav(SAMPLE), read_wav(SAMPLE_2)]
-    for r in reads:
+    for audio in Path(SAMPLE_DIR).iterdir():
+        r = read_wav(audio)
         basic_main(r)
